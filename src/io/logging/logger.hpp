8f2e738d42309cbda97123367461a2e1da120cc9
DcruBro <jonaskn914@gmail.com>
2025-05-16 20:50:13 +0200

    minor logger change

--- src/io/logging/logger.hpp
+++ src/io/logging/logger.hpp
@@ -2,18 +2,22 @@
 
 #include <iostream>
 
+#ifndef LOG
 #define LOG(msg) \
     std::cout << "\033[0m(LOG) " << __PRETTY_FUNCTION__ << ": " << msg << "\n"
+#endif
 
+#ifndef WARN
 #define WARN(msg) \
     std::cerr << "\033[33m(WARN) \033[0m" << __PRETTY_FUNCTION__ << ": " << msg << "\n"
+#endif
 
+#ifndef ERROR
 #define ERROR(msg) \
     std::cerr << "\033[31m(ERROR) \033[0m" << __PRETTY_FUNCTION__ << ": " << msg << "\n"
+#endif
 
+#ifndef DBG
 #define DBG(msg) \
     std::cerr << "\033[95m(DEBUG) \033[0m" << #msg /*<< " - " << __PRETTY_FUNCTION__*/ << ": " << msg << "\n"
-
-namespace GMTKEngine {
-    
-}
+#endif
